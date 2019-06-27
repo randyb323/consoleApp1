4002c8779c7eaf8cf49d7da558bbb4f54b706f92
Randy Burns <randy.burns@daimler.com>
2019-06-27 09:09:31 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+            }
+            catch(Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally Block");
+            }
+        }
+    }
+}
