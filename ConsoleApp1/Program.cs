2e48cc450993bee43a7d41ec2052b6a94daa9f8c
Randy Burns <randy.burns@daimler.com>
2019-06-27 09:41:18 -0700

    Commit 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,6 +17,7 @@ namespace ConsoleApp1
             }
             finally
             {
+                //Comment added 
                 Console.WriteLine("Finally Block");
             }
         }
